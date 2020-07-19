8531ddd9abee6f382963a4c278c8cb02f76459cb
YQDevelops <lowyewqing@gmail.com>
2020-07-19 15:38:32 +0800

    Fixed inability to type commands if on_message is allowed.

--- Main/bot.py
+++ Main/bot.py
@@ -12,37 +12,38 @@ load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
 
-bot = commands.Bot(command_prefix = '=')
-@bot.command(name='99')
-async def nine_nine(ctx):
-
-    brooklyn_99_quotes = [
-        'I\'m the human form of the 💯 emoji.',
-        'Bingpot!',
-        (
-            'Cool. Cool cool cool cool cool cool cool, '
-            'no doubt no doubt no doubt no doubt.'
-        ),
-    ]
-
-    response = random.choice(brooklyn_99_quotes)
-    await ctx.send(response)
-# @bot.event
-# async def on_ready():
-#     guild = discord.utils.find(lambda g:g.name == GUILD, bot.guilds)
-#     print(
-#         f'{bot.user} has connected to Discord!\n'
-#         f'{guild.name}(id: {guild.id})\n'
-#     )
-#     members=', '.join([member.name for member in guild.members])
-#     print(f'Guild Members:{members}')
+bot = commands.Bot(command_prefix = commands.when_mentioned_or('zim='))
+bot.remove_command('help')
+# @bot.command(name='99')
+# async def nine_nine(ctx):
+#
+#     brooklyn_99_quotes = [
+#         'I\'m the human form of the 💯 emoji.',
+#         'Bingpot!',
+#         (
+#             'Cool. Cool cool cool cool cool cool cool, '
+#             'no doubt no doubt no doubt no doubt.'
+#         ),
+#     ]
+#
+#     response = random.choice(brooklyn_99_quotes)
+#     await ctx.send(response)
+@bot.event
+async def on_ready():
+    guild = discord.utils.find(lambda g:g.name == GUILD, bot.guilds)
+    print(
+        f'{bot.user} has connected to Discord!\n'
+        f'{guild.name}(id: {guild.id})\n'
+    )
+    members=', '.join([member.name for member in guild.members])
+    print(f'Guild Members:{members}')
 #
 # @bot.event
 # async def on_member_join(member):
 #     await member.create_dm()
 #     await member.dm_channel.send(
-#         f'''Hi, welcome to the guild where I, the magnanimous,
-#          the magnificent, the amazing {bot.user.name} am tested!'''
+#         f'''Hi, welcome to the guild where I,
+#             {bot.user.name} am tested!'''
 #     )
 #
 @bot.event
@@ -58,49 +59,81 @@ async def on_message(message):
 
     pattern = re.compile(r'In [\d]+ (minute|hour|day)s?', re.I)
     match = pattern.finditer(message.content)
+
     if [matches for matches in match]!= []:
-        response = f'The Debate Will Start i{message.content[1:]}'
-        await message.channel.send(response, tts=True)
+        response = f'The Debate Will Start {message.content}'
+        await message.channel.send(response)
 
     if message.content == 'raise-exception':
         raise discord.DiscordException
 
+    await bot.process_commands(message)
+
 @bot.command(name='show')
-async def show_member(ctx):
+async def show_member(ctx,id=None):
+
 #error was caused by not defining guild
     guild = discord.utils.find(lambda g:g.name == GUILD, bot.guilds)
-
+    for guilds in bot.guilds:
+        print(guilds)
 #error was caused by not using name variable of instance
     members = ', '.join([members.name for members in guild.members])
-    message = members + f'the default role of this guild is \'{guild.default_role}\''
+    message = members + f' the default role of this guild is \'{guild.default_role.name[1:]}\''
     await ctx.send(message)
 
 @bot.command(name='send')
-async def send_dm(ctx):
+async def send_dm(ctx, role, msg=f'Hi. You are a member of '):
     guild = discord.utils.find(lambda g:g.name == GUILD, bot.guilds)
-    for member in guild.members:
-        if member.name == 'WeiQian':
+    rolesMentioned = discord.utils.find(lambda r:r, ctx.message.role_mentions)
+    if rolesMentioned != None:
+        print(rolesMentioned)
+        for member in rolesMentioned.members:
+            print(type(member))
             await member.create_dm()
             print('dm channel created!')
-            await member.dm_channel.send(f'Hi {member.name}!')
+            await member.dm_channel.send(f'{msg} {rolesMentioned.name}!')
             print('dm should have been sent!')#bot do not have dm_channel attribute
 
-@bot.command(name='end')
+@bot.command(name='end', brief='Command to shut me down', help='Invoke me by @ mentioning me and typing \'end\' after or typing \'zim=end\'. This will shut me down and you won\'t be able to command me anymore.')
 async def ending(ctx):
     await bot.logout()
 
 @bot.command(name='addroles')
-async def add_roles(ctx, id):
+async def add_roles(ctx, id=None):
     guild = discord.utils.find(lambda g:g.name == GUILD, bot.guilds)
-
     for roles in guild.roles:
         if str(roles) == 'ZimBot - Tester':
             role = roles
+
     for member in guild.members:
         if member.id == int(id):
             await member.add_roles(role)
-            # await bot.add_role(member, role) # dont use bot.add_role. Its old code!
+            # await bot.add_role(member, role) # dont use bot.add_role. Its outdated!
             await ctx.send(f'I changed {member.name}\'s role to "{str(role)}"')
+
+@bot.command(name='leave')
+async def leave_guild(ctx,id = None):
+    if id != None:
+        guildImLeaving = discord.utils.find(lambda g:g.id == int(id), bot.guilds)
+        print(guildImLeaving.name)
+        await guildLeaving.leave()
+
+    else:
+        guildImLeaving = ctx.guild
+        await guildImLeaving.leave()
+
+@bot.command(name='help', brief='A help command', help='A command to help give information to end users. Add "commands" after the help command to get a list of commands available to you.')
+async def help_info(ctx, *args):
+    parameters = {'title':'Mr.ZimBot', type:'rich', 'description':'The place to find out about me: Mr.ZimBot!', 'timestamp':'2020-09-27', 'color':255}
+    embeddedMessage=discord.Embed.from_dict(parameters)
+    for arg in args:
+        possibleCommand=discord.utils.find(lambda c:c.name == arg, bot.commands)
+        if arg=='commands':
+            embeddedMessage.add_field(name='help for commands',value='Commands:\n'+'\n'.join(f'{c.name}---{c.brief}' for c in bot.commands))
+        if possibleCommand != None:
+            embeddedMessage.add_field(name=possibleCommand.name,value=possibleCommand.help)
+    await ctx.send(embed=embeddedMessage)
+
 
 @bot.event
 async def on_error(event, *args, **kwargs):
